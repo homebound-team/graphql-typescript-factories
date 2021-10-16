d3fcd67a7b5c1ece02d0316f8e73ffca7eeb06dc
samigirma1 <sgz4>
2021-10-16 12:57:05 -0700

    enable separating type definitions for factories

---
+++ integration/graphql-types-only.ts
@@ -0,0 +1,123 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: any;
+};
+
+export type Author = Named & {
+  __typename?: 'Author';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  summary: AuthorSummary;
+  popularity: PopularityDetail;
+  workingDetail: WorkingDetail;
+  working?: Maybe<Working>;
+  birthday?: Maybe<Scalars['Date']>;
+  books: Array<Book>;
+};
+
+export type AuthorInput = {
+  name?: Maybe<Scalars['String']>;
+};
+
+export type AuthorSummary = {
+  __typename?: 'AuthorSummary';
+  id: Scalars['ID'];
+  author: Author;
+  numberOfBooks: Scalars['Int'];
+  amountOfSales?: Maybe<Scalars['Float']>;
+};
+
+export type Book = Named & {
+  __typename?: 'Book';
+  name: Scalars['String'];
+  popularity?: Maybe<PopularityDetail>;
+  coauthor?: Maybe<Author>;
+  reviews?: Maybe<Array<Maybe<BookReview>>>;
+};
+
+export type BookReview = {
+  __typename?: 'BookReview';
+  rating: Scalars['Int'];
+};
+
+export type CalendarInterval = {
+  __typename?: 'CalendarInterval';
+  start: Scalars['Date'];
+  end: Scalars['Date'];
+};
+
+export type Child = {
+  __typename?: 'Child';
+  parent: Named;
+};
+
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  saveAuthor: SaveAuthorResult;
+};
+
+
+export type MutationSaveAuthorArgs = {
+  input: AuthorInput;
+};
+
+export type Named = {
+  name: Scalars['String'];
+};
+
+export enum Popularity {
+  Low = 'Low',
+  High = 'High'
+}
+
+export type PopularityDetail = Named & {
+  __typename?: 'PopularityDetail';
+  code: Popularity;
+  name: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  authors: Array<Author>;
+  authorSummaries: Array<AuthorSummary>;
+  search: Array<SearchResult>;
+};
+
+
+export type QueryAuthorsArgs = {
+  id?: Maybe<Scalars['ID']>;
+};
+
+
+export type QuerySearchArgs = {
+  query: Scalars['String'];
+};
+
+export type SaveAuthorResult = {
+  __typename?: 'SaveAuthorResult';
+  author: Author;
+};
+
+export type SearchResult = Author | Book;
+
+export enum Working {
+  Yes = 'YES',
+  No = 'NO'
+}
+
+export type WorkingDetail = {
+  __typename?: 'WorkingDetail';
+  code: Working;
+  name: Scalars['String'];
+  extraField: Scalars['Int'];
+};
--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -1,83 +1,110 @@
 import { jan1 } from "./testData";
-import {
-  newAuthor,
-  newAuthorSummary,
-  newBook,
-  newCalendarInterval,
-  newChild,
-  Popularity,
-  resetFactoryIds,
-} from "./graphql-types";
+
+import * as TypesAndFactories from "./graphql-types"
+import * as TypesOnly from "./graphql-types-only"
+import * as FactoriesOnly from "./graphql-type-factories"
+
+const getTestObjects = (separateTypes: boolean = false) => {
+  if (separateTypes) {
+    return { ...TypesOnly, ...FactoriesOnly }
+  } else {
+    return TypesAndFactories
+  }
+}
+
+const getTests = (testType: "types-imported" | "types-in-file" = "types-in-file") => {
+  let testLabel: string
+  if (testType === "types-imported") {
+    testLabel = "Types and Factories in separate files"
+  } else {
+    testLabel = "Types and Factories in a shared file"
+  }
+
+  return describe(testLabel, () => {
+    const {
+      newAuthor,
+      newAuthorSummary,
+      newBook,
+      newCalendarInterval,
+      newChild,
+      Popularity,
+      resetFactoryIds,
+    } = getTestObjects(testType === "types-imported")
+  
+    it("does not infinite loop", () => {
+      const a = newAuthor({});
+      expect(a.name).toEqual("name");
+      expect(a.summary.author).toStrictEqual(a);
+    });
+  
+    it("auto-factories children", () => {
+      const a = newAuthor({
+        summary: { amountOfSales: 100 },
+        books: [{ name: "b1" }],
+      });
+      expect(a.__typename).toEqual("Author");
+      expect(a.summary.__typename).toEqual("AuthorSummary");
+      expect(a.books[0].__typename).toEqual("Book");
+    });
+  
+    it("creates unique ids", () => {
+      resetFactoryIds();
+      const a1 = newAuthor({});
+      const a2 = newAuthor({});
+      expect(a1.id).toEqual("a:1");
+      expect(a2.id).toEqual("a:2");
+      resetFactoryIds();
+      const a3 = newAuthor({});
+      expect(a3.id).toEqual("a:1");
+    });
+  
+    it("creates tagged ids based on config", () => {
+      resetFactoryIds();
+      const as = newAuthorSummary({});
+      expect(as.id).toEqual("summary:1");
+    });
+  
+    it("fills in type name of enums", () => {
+      const a = newAuthor({ popularity: { code: Popularity.Low } });
+      expect(a.popularity.__typename).toEqual("PopularityDetail");
+      expect(a.popularity.name).toEqual("Low");
+    });
+  
+    it("accepts codes for enum details when nested", () => {
+      const a = newAuthor({ books: [{ popularity: Popularity.Low }] });
+      expect(a.books[0].popularity?.name).toEqual("Low");
+    });
+  
+    it("can accept types as options with nullable references", () => {
+      const book = newBook();
+      const a = newAuthor({ books: [book] });
+    });
+  
+    it("can have defaults for custom scalars", () => {
+      const a = newCalendarInterval();
+      expect(a.start).toEqual(jan1);
+      expect(a.end).toEqual(jan1);
+    });
+  
+    it("picks the 1st impl of an interface", () => {
+      const c = newChild();
+      expect((c.parent as any).__typename).toEqual("Author");
+    });
+  
+    it("keeps property as undefined if option is explicitly set to undefined", () => {
+      const a = newAuthor({ summary: undefined });
+      expect(a.__typename).toEqual("Author");
+      expect(a.summary).toBeUndefined();
+    });
+  
+    it("generates property if option is not passed", () => {
+      const a = newAuthor({});
+      expect(a.summary.__typename).toEqual("AuthorSummary");
+    });
+  });
+}
 
 describe("typescript-factories", () => {
-  it("does not infinite loop", () => {
-    const a = newAuthor({});
-    expect(a.name).toEqual("name");
-    expect(a.summary.author).toStrictEqual(a);
-  });
-
-  it("auto-factories children", () => {
-    const a = newAuthor({
-      summary: { amountOfSales: 100 },
-      books: [{ name: "b1" }],
-    });
-    expect(a.__typename).toEqual("Author");
-    expect(a.summary.__typename).toEqual("AuthorSummary");
-    expect(a.books[0].__typename).toEqual("Book");
-  });
-
-  it("creates unique ids", () => {
-    resetFactoryIds();
-    const a1 = newAuthor({});
-    const a2 = newAuthor({});
-    expect(a1.id).toEqual("a:1");
-    expect(a2.id).toEqual("a:2");
-    resetFactoryIds();
-    const a3 = newAuthor({});
-    expect(a3.id).toEqual("a:1");
-  });
-
-  it("creates tagged ids based on config", () => {
-    resetFactoryIds();
-    const as = newAuthorSummary({});
-    expect(as.id).toEqual("summary:1");
-  });
-
-  it("fills in type name of enums", () => {
-    const a = newAuthor({ popularity: { code: Popularity.Low } });
-    expect(a.popularity.__typename).toEqual("PopularityDetail");
-    expect(a.popularity.name).toEqual("Low");
-  });
-
-  it("accepts codes for enum details when nested", () => {
-    const a = newAuthor({ books: [{ popularity: Popularity.Low }] });
-    expect(a.books[0].popularity?.name).toEqual("Low");
-  });
-
-  it("can accept types as options with nullable references", () => {
-    const book = newBook();
-    const a = newAuthor({ books: [book] });
-  });
-
-  it("can have defaults for custom scalars", () => {
-    const a = newCalendarInterval();
-    expect(a.start).toEqual(jan1);
-    expect(a.end).toEqual(jan1);
-  });
-
-  it("picks the 1st impl of an interface", () => {
-    const c = newChild();
-    expect((c.parent as any).__typename).toEqual("Author");
-  });
-
-  it("keeps property as undefined if option is explicitly set to undefined", () => {
-    const a = newAuthor({ summary: undefined });
-    expect(a.__typename).toEqual("Author");
-    expect(a.summary).toBeUndefined();
-  });
-
-  it("generates property if option is not passed", () => {
-    const a = newAuthor({});
-    expect(a.summary.__typename).toEqual("AuthorSummary");
-  });
+  getTests("types-in-file");
+  getTests("types-imported")
 });
